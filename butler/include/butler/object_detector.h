8af2552cf9539ade27030e33258b80b6516c782f
ventov <marcusventovaara@gmail.com>
2017-11-30 10:26:04 +0100

    fixed k-means and cup cluster selection

--- butler/include/butler/object_detector.h
+++ butler/include/butler/object_detector.h
@@ -123,6 +123,9 @@ class ObjectDetector
 			short m = 0;
 			for(const auto& bb : result->boundingBoxes.boundingBoxes) 
 			{
+				/*
+				 * reshape bounding box pixels into a 1D array for k-means
+				 */
 				cv::Mat obj((bb.xmax - bb.xmin) * (bb.ymax - bb.ymin), 1, CV_32F);
 				for(int k = 0; k < (bb.xmax - bb.xmin); ++k)
 				{
@@ -137,34 +140,45 @@ class ObjectDetector
 				/*
 				 * K-Means clustering to find a decent depth to the detected object
 				 */
-				cv::kmeans(obj, 2, labels, 
-						cv::TermCriteria(CV_TERMCRIT_ITER | CV_TERMCRIT_EPS, 10, 1.0), 
+				cv::kmeans(obj, 3, labels, 
+						cv::TermCriteria(CV_TERMCRIT_ITER | CV_TERMCRIT_EPS, 50, 0.1), 
 						3, cv::KMEANS_PP_CENTERS, centers);
 
-				float dist = centers.at<float>(0, 0);
-				float avg = 0.0;
+				float dist = 0.0,	//k-means distanec
+					  avg = 0.0;	//threshholded average distance
 				int count = 0;
-				for(int k = 0; k < (bb.xmax - bb.xmin); ++k)
-				{
-					for(int l = 0; l < (bb.ymax - bb.ymin); ++l)
-					{
-						avg += thresh(oldDepth_->image.at<unsigned char>(l + bb.ymin, k + bb.xmin), count, 0.1);
-						int idx = labels.at<int>(l + (k * (bb.ymax - bb.ymin)), 0);
-						oldDepth_->image.at<unsigned char>(l + bb.ymin, k + bb.xmin) = (unsigned char) (centers.at<float>(idx, 0) * 255);
-						if(dist < centers.at<float>(idx, 0)) dist = centers.at<float>(idx, 0);
-					}
-				}
+				/*
+				 * calculate average of thresholded pixel values for distance estimate
+				 * also draw the clustered colors onto the original image
+				 */
+			//	for(int k = 0; k < (bb.xmax - bb.xmin); ++k)
+			//	{
+			//		for(int l = 0; l < (bb.ymax - bb.ymin); ++l)
+			//		{
+			//			avg += thresh(oldDepth_->image.at<unsigned char>(l + bb.ymin, k + bb.xmin), count, 0.1);
+			//			int idx = labels.at<int>(l + (k * (bb.ymax - bb.ymin)), 0);
+			//			oldDepth_->image.at<unsigned char>(l + bb.ymin, k + bb.xmin) = (unsigned char) (centers.at<float>(idx, 0) * 255);
+			//		}
+			//	}
+				
+				dist = mean(centers.at<float>(0), centers.at<float>(1), centers.at<float>(2));
+				
+				ROS_INFO("centers: %f, %f, %f", centers.at<float>(0), centers.at<float>(1), centers.at<float>(2));
 				avg /= (float) count;
 
 				std::ostringstream s;
 				s << std::setprecision(3) << m << "; P: " << bb.probability;
 
+				/*
+				 * draw bounding boxes on depth image
+				 */
 				cv::putText(oldDepth_->image, s.str(), cv::Point(bb.xmin, bb.ymin), cv::FONT_HERSHEY_PLAIN, 1.0, 1.0);
 				cv::line(oldDepth_->image, cv::Point(bb.xmin, bb.ymin), cv::Point(bb.xmin, bb.ymax), 1.0, 2);
 				cv::line(oldDepth_->image, cv::Point(bb.xmin, bb.ymax), cv::Point(bb.xmax, bb.ymax), 1.0, 2);
 				cv::line(oldDepth_->image, cv::Point(bb.xmax, bb.ymin), cv::Point(bb.xmax, bb.ymax), 1.0, 2);
 
-				ROS_INFO("#%d: %s; Prb: %3f, K: %3f, A: %3f", ++m, bb.Class.c_str(), bb.probability, dist, avg);
+				//ROS_INFO("#%d: %s; Prb: %3f, K: %3f, A: %3f", ++m, bb.Class.c_str(), bb.probability, dist, avg);
+				ROS_INFO("#%d: %s; Prb: %3f, K: %3f", ++m, bb.Class.c_str(), bb.probability, dist);
 			}
 			cv::imshow("depthwin", oldDepth_->image);
 			char wKey = cv::waitKey(1) & 0xFF;
@@ -184,7 +198,10 @@ class ObjectDetector
 		sensor_msgs::Image rgbImg_, depthImg_;
 		cv_bridge::CvImagePtr oldDepth_;
 
-		inline float thresh(const unsigned char& d, int& c, const float v)
+		/*
+		 * threshholding function, also keeps track of "successful" addition with int& c
+		 */
+		inline float thresh(const unsigned char d, int& c, const float v)
 		{
 			if((float) (d / 255.0) > v) 
 			{
@@ -193,6 +210,15 @@ class ObjectDetector
 			}
 			else return 0.0;
 		}
+
+		template<typename T>
+			inline T mean(T x, T y, T z)
+			{
+				if(((x < y) && (x > z)) || ((x < z) && (x > y))) return x;
+				if(((y < x) && (y > z)) || ((y < z) && (y > x))) return y;
+				if(((z < x) && (z > y)) || ((z < y) && (z > x))) return z;
+			}
+		
 
 };
 
